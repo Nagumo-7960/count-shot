51122907ad32fb02c42e9f050fa00180a0a008ef
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2024-10-13 04:49:52 +0900

    UIを改善してリセットボタンを追加(リセット機能未実装)

--- count-shot/app/src/main/java/com/example/countshot/presentation/CountScreen.kt
+++ count-shot/app/src/main/java/com/example/countshot/presentation/CountScreen.kt
@@ -2,18 +2,25 @@ package com.example.countshot.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 
@@ -34,19 +41,37 @@ private fun CountContent(
     count: Int,
     onClicked: () -> Unit
 ) {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.background),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             modifier = Modifier.clickable { onClicked() },
             text = count.toString(),
-            fontSize = 38.sp,
+            fontSize = 48.sp,
             textAlign = TextAlign.Center,
             color = MaterialTheme.colors.primary,
         )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Button(
+            modifier = Modifier.size(
+                width = 56.dp,
+                height = 28.dp
+            ),
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(10),
+        ) {
+            Text(
+                text = "RESET",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
 
